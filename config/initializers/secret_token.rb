5f9383228613a96cb6ec6cb07e2fc47030f4c103
kn65op <kn65op@gmail.com>
2012-11-01 20:30:58 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+WebDomag::Application.config.secret_token = '4e1992ddc9cba3b79ab04979d6510f2f59b2a66eb2b1ee36c7807c8a0b247731ec082b6cca86dbb122a666c769d8cd7ca8e83079e680856740156ab43dfade28'
