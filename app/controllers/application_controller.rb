76a1a7e0df0ec224cecfa43bca011fa4e93b51d1
kn65op <kn65op@gmail.com>
2012-11-11 13:09:00 +0100

    remove bad changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-	
-	def default_url_options(options={})
-  	logger.debug "default_url_options is passed options: #{options.inspect}\n"
-  	{ :locale => I18n.locale }
-	end
 
 	def main_page
 		
