958cbae1d67e5f2a2450cfac68b9fef92e676b01
kn65op <kn65op@gmail.com>
2013-01-12 00:51:03 +0100

    Add test for authenticating in users controller

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -4,10 +4,32 @@ class UsersControllerTest < ActionController::TestCase
   # test "the truth" do
   #   assert true
   # end
+	include Devise::TestHelpers
 
-	test "should get index" do
-		get "settings" 
-  	assert_response :success
-		assert_not_nil assigns(:post)
+#main page
+	
+	test "should get index without user" do
+		get :main_page, :locale => :pl
+  	assert_response :success, "Not success"
+#		assert_not_nil assigns(:posts), "Nill asssigns"
 	end
+
+	test "should get index with user" do
+		sign_in users(:one)
+		get :main_page, :locale => :pl
+  	assert_response :success, "Not success"
+	end
+
+#settings
+	test "should not get settings without user" do
+		get :settings, :locale => :pl
+  	assert_redirected_to new_user_session_pl_path, "Wrong success"
+	end
+
+	test "should get settings with user" do
+		sign_in users(:one)
+		get :settings, :locale => :pl
+  	assert_response :success, "Not success"
+	end
+
 end
