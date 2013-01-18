89ed5fd2dd4cd3cbd08f3faf06e162740c84ca87
kn65op <kn65op@gmail.com>
2013-01-18 22:52:31 +0100

    Second part storages test

--- test/unit/storages_test.rb
+++ test/unit/storages_test.rb
@@ -4,4 +4,15 @@ class StoragesTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+# Error due to database.
+#  test "No new storage invalid" do
+#    storage = Storage.new()
+#    assert !storage.save, "Invalid storage saved"
+#  end
+
+  test "Valid storage save" do
+    storage = Storage.new()
+    storage.name = "a"
+    assert storage.save, "Valid storage not saved"
+  end
 end
