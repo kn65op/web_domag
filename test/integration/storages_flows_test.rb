00abab1af09f04183bf71f5ffb9ecd74db567124
kn65op <kn65op@gmail.com>
2013-01-19 11:08:20 +0100

    test for later

--- test/integration/storages_flows_test.rb
+++ test/integration/storages_flows_test.rb
@@ -35,6 +35,21 @@ class StoragesFlowsTest < ActionDispatch::IntegrationTest
 
   end
 
+# adding user permissions will be add later
+#  test "Add storage, add permission to view to other user, check if user can view" do
+#    new_storage = "Nowy schowek"
+#    new_description = "Nowy opis"
+#
+#    https!
+#    login_as(users(:one))
+#
+#    visit "/pl/storage/new"
+#    page.has_content?("Dodaj nowy schowek")
+#
+#    fill_in("Nazwa", :with => new_storage)
+#    fill_in("Opis", :with => new_description)
+#  end
+
 #    current_path.should == user_settings()
 #    assert_equal "en", find_by_id('lang_select').find('option[selected]').value, "Wrong selected lang"
 
