3cb6af3c58cbc80786f5969a4d93614d62c01025
kn65op <kn65op@gmail.com>
2013-01-19 23:17:57 +0100

    Add thing validations

--- app/models/thing.rb
+++ app/models/thing.rb
@@ -6,4 +6,9 @@ class Thing < ActiveRecord::Base
 	has_many :limits
 	has_many :thing_instances
 	belongs_to :category
+
+  #validations
+  validate :name, :presence => true
+  validate :category_id, :presence => true
+  validate :unit, :presence => true
 end
