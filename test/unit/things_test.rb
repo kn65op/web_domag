db8e06721c002500a9ec0c0764442a4373305c00
kn65op <kn65op@gmail.com>
2013-01-25 23:27:29 +0100

    Thing unit tests

--- test/unit/things_test.rb
+++ test/unit/things_test.rb
@@ -4,4 +4,22 @@ class ThingsTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  test "Add new thing" do
+    thing = Thing.new(:name => "New thing")
+    thing.category = categories(:one_drinks)
+    thing.unit = "unit"
+    assert thing.save, "Thing not saved"
+  end
+
+  test "Can edit my thing" do
+    thing = things(:one_beer)
+    assert thing.canView?(users(:one)), "cannot edit"
+  end
+
+  test "Can't edit not my thing" do
+    thing = things(:two_beer)
+    assert !thing.canView?(users(:one)), "can edit"
+  end
+
 end
