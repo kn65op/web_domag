9fcbe317855a53efe2e90e1521c0cab26eb81f70
kn65op <kn65op@gmail.com>
2013-01-26 13:02:31 +0100

    shops andmanufacturers controllers

--- app/controllers/manufacturers_controller.rb
+++ app/controllers/manufacturers_controller.rb
@@ -3,22 +3,41 @@ class ManufacturersController < ApplicationController
   before_filter :canView?, :only => [:edit, :delete, :confirmed_delete, :view]
 
   def index
+    @manufactures = current_user.manufacturers
   end
 
   def new
+    if params[:manufacturer] != nil #Save to database
+      @manufacturer = Manufacturer.new(params[:manufacturer]) #create new manufacturer
+      @manufacturer.user = current_user
+      if @manufacturer.save #no errors on save, redirect
+        redirect_to manufacturers_path, :notice => t('manufacturers.new.successful')
+      end
+    else
+      @manufacturer = Manufacturer.new
+    end
   end
 
   def edit
+    @manufacturer = getManufacturer
+    if params[:manufacturer] != nil && @manufacturer.update_attributes(params[:manufacturer])
+      redirect_to view_manufacturer_path(@manufacturer), :notice => t('manufacturers.edit.successful')
+    end
   end
 
   def view
-
+    @manufacturer = getManufacturer
   end
 
   def delete
+    @manufacturer = getManufacturer
   end
 
   def confirmed_delete
+    manufacturer = getManufacturer
+    manufacturer.delete
+
+    redirect_to manufacturers_path, :notice => t('manufacturers.delete.succesful')
   end
 
 private
--- app/controllers/shops_controller.rb
+++ app/controllers/shops_controller.rb
@@ -3,22 +3,41 @@ class ShopsController < ApplicationController
   before_filter :canView?, :only => [:edit, :delete, :confirmed_delete, :view]
 
   def index
+    @shops = current_user.shops
   end
 
   def new
+    if params[:shop] != nil #Save to database
+      @shop = Shop.new(params[:shop]) #create new shop
+      @shop.user = current_user
+      if @shop.save #no errors on save, redirect
+        redirect_to shops_path, :notice => t('shops.new.successful')
+      end
+    else
+      @shop = Shop.new
+    end
   end
 
   def edit
+    @shop = getShop
+    if params[:shop] != nil && @shop.update_attributes(params[:shop])
+      redirect_to view_shop_path(@shop), :notice => t('shops.edit.successful')
+    end
   end
 
   def view
-
+    @shop = getShop
   end
 
   def delete
+    @shop = getShop
   end
 
   def confirmed_delete
+    shop = getShop
+    shop.delete
+
+    redirect_to shops_path, :notice => t('shops.delete.succesful')
   end
 
 private
