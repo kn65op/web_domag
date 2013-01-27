78df8c8b3925cc21ea4a36fc530fd1e902874e89
kn65op <kn65op@gmail.com>
2013-01-27 09:53:37 +0100

    Test for thing instance and amount

--- test/unit/thing_instances_test.rb
+++ test/unit/thing_instances_test.rb
@@ -4,4 +4,15 @@ class ThingInstancesTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+  #amount
+  test "amunt == size" do
+    ti = thing_instances(:one_beer1)
+    assert_equal ti.size, ti.amount, "Amount != size"
+  end
+
+  test "Consumed" do
+  ti = thing_instances(:one_beer2)
+  assert_equal 1.5, ti.amount, "Amount was not 1.5"
+  end
 end
